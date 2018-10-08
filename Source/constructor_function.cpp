952e2ad1366b9496b4a1718df7bfeada1b96290c
marufurrahman <marufurrahman1997@gmail.com>
2018-10-08 21:44:26 +0600

    constructor function

---
+++ Source/constructor_function.cpp
@@ -0,0 +1,29 @@
+// Constructor functions
+// Written by Marufur Rahman
+
+#include <iostream>
+
+using namespace std;
+
+class Book {
+	public:
+		string title;
+		string author;
+		int pages;
+
+		Book(string aTitle, string aAuthor, int aPages) {
+			title = aTitle;
+			author = aAuthor;
+			pages = aPages;
+		}
+};
+
+int main()
+{
+	Book book1("Learn Python the Hard Way", "Zed Shaw", 769);
+	Book book2("Head First Java", "Bert Bates and Kathy Sierra", 701);
+
+	cout << book1.title;
+
+	return 0;
+}
